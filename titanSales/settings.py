50ac4e2c67869878bfdfa91bc9ab8049fa9b00ac
sosthenes-py <sosthenes.wedigital@gmail.com>
2024-02-10 03:57:19 +0100

    collected statics

--- titanSales/settings.py
+++ titanSales/settings.py
@@ -124,6 +124,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'landing/static/landing'
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
