a3792c8504c035508209885ba9023e13bb822129
sosthenes-py <sosthenes.wedigital@gmail.com>
2024-06-12 12:31:35 +0100

    latest commit

--- titanSales/settings.py
+++ titanSales/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'titansales.onrender.com']
 
 
 
